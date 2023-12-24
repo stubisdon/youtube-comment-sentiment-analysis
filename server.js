7a4a48d50002d1e744b09ddf3b179dfa140e61e4
Keroko <stub@Kerokos-MBP-8.lan>
2023-12-23 19:51:42 -0800

    add success and error cases handling

--- server.js
+++ server.js
@@ -7,13 +7,26 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '.')));
 
 app.post('/analyze', (req, res) => {
-        const videoUrl = req.body.url;
-        // Extract video ID from URL...
-        // Fetch comments...
-        // Perform sentiment analysis...
-        // Calculate percentages...
-        // Send results back to frontend...
-        res.json({ positive: 50, negative: 50 });
+    const videoUrl = req.body.url;
+    
+    let videoId;
+    try {
+        // Extract video ID from URL
+        const url = new URL(videoUrl);
+        videoId = new URLSearchParams(url.search).get('v');
+    } catch (error) {
+        return res.status(400).json({ error: 'Invalid URL' });
+    }
+
+    if (!videoId) {
+        return res.status(400).json({ error: 'Invalid YouTube URL. The URL should be in the format: https://www.youtube.com/watch?v=videoId' });
+    }
+
+    // Fetch comments...
+    // Perform sentiment analysis...
+    // Calculate percentages...
+    // Send results back to frontend...
+    res.json({ positive: 50, negative: 50 });
 });
 
 app.get('/', (req, res) => {
