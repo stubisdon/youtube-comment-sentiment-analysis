7a19383fff0d57913a5d2903518a88a21678a274
Keroko <stub@Kerokos-MBP-8.lan>
2023-12-23 20:33:56 -0800

    added actual comment fetching

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+require('dotenv').config();
+const { google } = require('googleapis');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '.')));
@@ -22,11 +24,46 @@ app.post('/analyze', (req, res) => {
         return res.status(400).json({ error: 'Invalid YouTube URL. The URL should be in the format: https://www.youtube.com/watch?v=videoId' });
     }
 
-    // Fetch comments...
+    console.log('the ID is: ' + videoId);
+
+    const { google } = require('googleapis');
+
+    console.log('Loading Google API...');
+
+    // Initialize the YouTube API client
+    const youtube = google.youtube({
+        version: 'v3',
+        auth: process.env.YOUTUBE_API_KEY
+    });
+
+    console.log('Google API loaded. API Key is ' + process.env.YOUTUBE_API_KEY);
+
+    // Fetch comments
+    youtube.commentThreads.list({
+        part: 'snippet',
+        videoId: videoId,
+        maxResults: 100, // Fetch up to 100 comments
+    }, (err, response) => {
+        if (err) {
+            console.error('The API returned an error: ' + err);
+            res.status(500).json({ error: 'The API returned an error: ' + err });
+            return;
+        }
+
+        const comments = response.data.items.map(item => item.snippet.topLevelComment.snippet.textDisplay);
+
+        console.log('Comments fetched. the number of comments is: ' + comments.length);
+
+        // TODO: Analyze the comments...
+
+        res.json({ comments: comments });
+    });
+
+    console.log('Cycle of fetch comments complete...');
+
     // Perform sentiment analysis...
     // Calculate percentages...
     // Send results back to frontend...
-    res.json({ positive: 50, negative: 50 });
 });
 
 app.get('/', (req, res) => {
