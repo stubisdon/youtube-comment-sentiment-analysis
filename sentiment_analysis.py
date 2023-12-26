c0de74691eeb8922646d19a6fb3b2f686338b1cc
Keroko <stub@Kerokos-MBP-24.lan>
2023-12-26 11:21:01 -0800

    added error handling to python script

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import traceback
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import nltk
 nltk.download('vader_lexicon')
@@ -9,11 +10,17 @@ def analyze_sentiment(comment):
     sentiment_scores = sid.polarity_scores(comment)
     return sentiment_scores['compound']
 
-# Read the comments from stdin
-comments = json.load(sys.stdin)
+try:
+    # Read the comments from stdin
+    comments = json.load(sys.stdin)
 
-# Analyze each comment
-sentiment_results = [{'score': analyze_sentiment(comment), 'text': comment} for comment in comments]
+    # Analyze each comment
+    sentiment_results = [{'score': analyze_sentiment(comment), 'text': comment} for comment in comments]
 
-# Write the sentiment results to stdout
-json.dump(sentiment_results, sys.stdout)
+    # Write the sentiment results to stdout
+    json.dump(sentiment_results, sys.stdout)
+
+except Exception as e:
+    # Print the error message and traceback to stderr
+    print(f"An error occurred: {str(e)}", file=sys.stderr)
+    traceback.print_exc(file=sys.stderr)
